a5120f9e277b9f441ff4d8ed64af0cd030cd560a
heavyrisem <pyo1748@gmail.com>
2022-04-01 21:57:59 +0900

    fix

--- LongvinterUpdate.sh
+++ LongvinterUpdate.sh
@@ -6,7 +6,7 @@ UpdateServer () {
         sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
         git restore .
         sleep 1
-        git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" main
+        git pull "https://github.com/Buriburi223/longvinter-linux-server.git" main
         sleep 1
         echo "Starting server..."
         sudo chmod -R ugo+rwx ~/longvinter-linux-server/
